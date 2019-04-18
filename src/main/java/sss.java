2187bd4b76193da30c3c4d4192391236e5052a0f
github1051890370 <github13733796560@163.com>
2019-04-18 17:08:51 +0800

    修改了hello

---
+++ src/main/java/sss.java
@@ -0,0 +1,12 @@
+/**
+ * fileName:sss
+ * description:
+ * author:lzy
+ * createTime:2019-04-18 12:28
+ */
+public class sss {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("修改了");
+    }
+}
